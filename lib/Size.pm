014a2a6dea368aa04c22e50b500f009659cf2055
brian d foy <brian.d.foy@gmail.com>
2004-07-04 17:04:17 +0000

    * added note about source availability on Sourceforge * updated copyright info * get_sizes() now always returns a hash ref in list context: not just for HTML pages * TO DO! get_sizes should also download javascript and style sheets
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1260 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Size.pm
+++ lib/Size.pm
@@ -8,9 +8,9 @@ HTTP::Size - Get the byte size of an internet resource
 =head1 SYNOPSIS
 
 	use HTTP::Size
-	
+
 	my $size = HTTP::Size::get_size( $url );
-	
+
 	if( defined $size )
 		{
 		print "$url size was $size";
@@ -27,7 +27,7 @@ HTTP::Size - Get the byte size of an internet resource
 		{
 		print "Could not determine content length of $url";
 		}
-	
+
 =head1 DESCRIPTION
 
 =head1 VARIABLES
@@ -37,7 +37,7 @@ function call:
 
 	$ERROR
 	$HTTP_STATUS
-	
+
 The C<$ERROR> variable may be set to any of these values:
 
 	$INVALID_URL	    - the URL is not a valid absolute URL
@@ -49,18 +49,18 @@ the full package specification outside of the HTTP::Size
 package.
 
 =cut
-	
+
 use subs qw( get_size _request );
-use vars qw( 
+use vars qw(
 	$ERROR $HTTP_STATUS $VERSION
-	$INVALID_URL $COULD_NOT_FETCH $BAD_CONTENT_TYPE 
+	$INVALID_URL $COULD_NOT_FETCH $BAD_CONTENT_TYPE
 	);
 
 use LWP::UserAgent;
 use URI;
 use HTTP::Request;
 
-$VERSION = 0.4;
+$VERSION = 0.9;
 
 my $User_agent = LWP::UserAgent->new();
 
@@ -78,7 +78,7 @@ Fetch the specified absolute URL and return its content length.
 The URL can be a string or an URI object.  The function tries
 the HEAD HTTP method first, and on failure, tries the GET method.
 In either case it sets $HTTP_STATUS to the HTTP response code.
-If the response does not contain a Content-Length header, the 
+If the response does not contain a Content-Length header, the
 function takes the size of the message body.  If the HEAD method
 returned a good status, but no Content-Length header, it retries
 with the GET method.
@@ -96,37 +96,37 @@ sub get_size
 	my $url    = shift;
 	my $method = shift || 0;
 	_init();
-	
+
 	unless( ref $url eq 'URI' )
 		{
 		$url = URI->new( $url );
 		}
-		
+
 	unless( $url->scheme )
 		{
 		$ERROR = $INVALID_URL;
 		return;
 		};
-	
+
 	my $response = '';
 	my $size     = 0;
-	
+
 	unless( $method )
 		{
 		my $request = HTTP::Request->new( HEAD => $url->as_string );
-			
+
 		$response    = _request( $request );
 		$HTTP_STATUS = $response->code;
 		$size        = $response->content_length;
 		}
-		
+
 	unless( not $method and $response->is_success and $size )
 		{
 		$request     = HTTP::Request->new( GET => $url->as_string );
 		$response    = _request( $request );
 		$HTTP_STATUS = $response->code;
 		$CONTENT     = $response->content;
-	
+
 		unless( $response->is_success )
 			{
 			$ERROR = $COULD_NOT_FETCH;
@@ -140,42 +140,44 @@ sub get_size
 			{
 			$size = $response->content_length;
 			}
-			
+
 		}
-	
+
 	$CONTENT_TYPE = lc $response->content_type;
 
-	return $size;	
+	return $size;
 	}
-	
+
 =item get_sizes( URL, BASE_URL )
 
 The get_sizes function is like get_size, although for HTML pages
-it also fetches all of the images then sums the sizes of the 
+it also fetches all of the images then sums the sizes of the
 original page and image sizes. It returns a total download size.
 In list context it returns the total download size and a hash
-reference whose keys are the URLs of the images found in the HTML
-and whose values are hash references with these keys:
+reference whose keys are the URLs that a browser should download
+automatically (images):
 
 	size
 	ERROR
 	HTTP_STATUS
-	
-The ERROR and HTTP_STATUS correspond to the values of $ERROR and 
+
+The ERROR and HTTP_STATUS correspond to the values of $ERROR and
 $HTTP_STATUS for that URL.
 
 	my ( $total, $hash ) = HTTP::Size::get_sizes( $url );
-	
+
 	foreach my $key ( keys %$hash )
 		{
 		print "$key had an error" unless defined $size;
 		}
 
-The hash will be empty if the initial resource does not return
-a 'text/html' MIME type.
+The hash is always returned in list context (a change from
+version 0.4).
 
 Relative image links resolve accroding to BASE_URL, or by
 a found BASE tag.  See L<HTML::SimpleLinkExtor>.
+
+Javascript and style sheet links are unimplemented right now.
 
 =cut
 
@@ -183,56 +185,51 @@ sub get_sizes
 	{
 	my $url  = shift;
 	my $base = shift;
-	
-	my %hash;  
-	
+
+	my %hash;
+
 	my $size = get_size( $url, 'GET' );
-	
-	@{$hash{$url}}{ qw(size ERROR HTTP_STATUS) } 
+
+	@{$hash{$url}}{ qw(size ERROR HTTP_STATUS) }
 		= ($size, $ERROR, $HTTP_STATUS);
-	
-	return $size unless( $size and $CONTENT_TYPE eq 'text/html' );
+
+	unless( $size and $CONTENT_TYPE eq 'text/html' )
+		{
+		return wantarray ? ( $size, \%hash ) : $size;
+		}
 
 	require HTML::SimpleLinkExtor;
-	
+
 	my $total = $size;
-	
+
 	my $extor = HTML::SimpleLinkExtor->new( $url );
-	
+
 	$extor->parse( $CONTENT );
-	
+
 	foreach my $img ( $extor->img )
 		{
 		my $size = get_size( $img ) || 0;
 
-		@{$hash{$img}}{ qw(size ERROR HTTP_STATUS) } 
-			= ($size, $ERROR, $HTTP_STATUS);
-		
+		@{$hash{$img}}{ qw(size ERROR HTTP_STATUS) }
+			= ( $size, $ERROR, $HTTP_STATUS );
+
 		$total += $size;
 		}
 
-	if( wantarray )
-		{
-		return ( $total, \%hash );
-		}
-	else
-		{
-		return $total;
-		}
-		
+	return wantarray ? ( $total, \%hash ) : $total;
 	}
 
 sub _init
 	{
 	$ERROR = $CONTENT_TYPE = $CONTENT = $HTTP_STATUS = '';
 	}
-	
+
 sub _request
 	{
 	my $response = $User_agent->request( shift );
-	
+
 	$HTTP_STATUS = $response->code;
-	
+
 	return $response;
 	}
 
@@ -245,9 +242,21 @@ downloading the whole thing
 
 * add a way to specify Basic Auth credentials
 
+* download javascript and style sheets too.
+
 =head1 SEE ALSO
 
 L<HTML::SimpleLinkExtor>
+
+=head1 SOURCE AVAILABILITY
+
+This source is part of a SourceForge project which always has the
+latest sources in CVS, as well as all of the previous releases.
+
+	http://sourceforge.net/projects/brian-d-foy/
+
+If, for some reason, I disappear from the world, one of the other
+members of the project can shepherd this module appropriately.
 
 =head1 AUTHOR
 
@@ -255,7 +264,7 @@ brian d foy <bdfoy@cpan.org>
 
 =head1 COPYRIGHT
 
-Copyright 2002, brian d foy, All Rights Reserved.
+Copyright (c) 2000-2004, brian d foy, All Rights Reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
