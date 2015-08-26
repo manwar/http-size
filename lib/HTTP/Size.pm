a7fb7d33b0af2ef36fe2d76d861a3ebe5a9ea1b9
brian d foy <brian.d.foy@gmail.com>
2015-08-26 11:42:27 -0400

    The project is now in GitHub

--- lib/HTTP/Size.pm
+++ lib/HTTP/Size.pm
@@ -253,13 +253,9 @@ L<HTML::SimpleLinkExtor>
 
 =head1 SOURCE AVAILABILITY
 
-This source is part of a SourceForge project which always has the
-latest sources in CVS, as well as all of the previous releases.
+This source is part of a GitHub project:
 
-	http://sourceforge.net/projects/brian-d-foy/
-
-If, for some reason, I disappear from the world, one of the other
-members of the project can shepherd this module appropriately.
+	https://github.com/briandfoy/http-size
 
 =head1 AUTHOR
 
